ec8219b87a83c13d0cab4fcf1d5d7b409166d662
ihashirr <ihashirr@gmail.com>
2026-03-20 01:49:21 +0400

    chore: flatten project structure, fix .gitignore

--- src/components/ConvexClientProvider.jsx
+++ src/components/ConvexClientProvider.jsx
@@ -1,8 +1,5 @@
 "use client";
-
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import { ReactNode } from "react";
-
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export default function ConvexClientProvider({ children }) {
